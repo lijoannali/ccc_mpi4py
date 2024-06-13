1c0a7d7259e817e732f843b6ef843709927ab722
Joanna Li <joannali.jli@gmail.com>
2024-06-13 10:39:12 -0400

    Changed recv buffer to np.empty with size and dtype

--- mpi4py-practice/scatterGather.py
+++ mpi4py-practice/scatterGather.py
@@ -10,40 +10,32 @@ rank = comm.Get_rank()
 n_jobs = size 
 
 #Lengths of local input chunk 
-local_n = np.zeros(1, dtype=int) 
-local_n_ccc = np.zeros(1, dtype=int) 
+local_n = np.empty(1, dtype=int) 
+local_n_ccc = np.empty(1, dtype=int) 
 
-#Allocate recv buffers
-local_input = np.ndarray((1), dtype=int)
-local_input_ccc = np.ndarray((1), dtype=int) 
 
 # Inputs to be scattered
-inputs = (np.array([0]), np.array([1]))
-inputs_ccc = (np.array([0])) #hardcoded to make 2 chunks
-print("Inputs", inputs)
-print("Inputs_ccc", inputs_ccc)
-
-#Pad an input, just for testing on 2 procs
-if (len(inputs_ccc) == 1): 
-    inputs_ccc.append(np.array([1]))
-    print("type is", type(inputs_ccc), len(inputs_ccc))
+inputs = np.array([0,1, 2,3 ], dtype=int)
+inputs_ccc = np.array([4, 5, 6, 7], dtype=int) #hardcoded to make 2 chunks
 
 if rank == 0: 
-    #Allocate results array
-    local_n = np.array([1]) #hardcoded to length of 1
-    local_n_ccc = np.array([1])
+    #Set the size of results
+    local_n = np.array([2]) #hardcoded
+    local_n_ccc = np.array([2])
 
 #Send size = [1] to all ranks
 comm.Bcast(local_n, root=0)
 comm.Bcast(local_n_ccc, root=0)
 
-#All ranks: 
+#Allocate recv buffers
+local_input = np.empty([size, local_n[0]], dtype=int)
+local_input_ccc = np.empty([size, local_n_ccc[0]], dtype=int)
 #Scatter input to procs by rank
 comm.Scatter(inputs, local_input, 0)
-comm.Scatter(inputs_ccc, local_input_ccc, 0)
+# comm.Scatter(inputs_ccc, local_input_ccc, 0)
 
 print("Input, rank", rank, "=", local_input)
-print("Input_ccc, rank", rank, "=", local_input)
+# print("Input_ccc, rank", rank, "=", local_input_ccc)
 
 
 #Local computation here
